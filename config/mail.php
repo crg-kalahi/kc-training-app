5aa4e5215da96f751410ce627d1bfa11d4413288
Jowam <69232719+dioame@users.noreply.github.com>
2025-07-07 14:18:54 +0800

    updates

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'host' => env('XMAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('XMAIL_USERNAME','djcrendon@dswd.gov.ph'),
+            'password' => env('XMAIL_PASSWORD', 'pluoskguqxzautaq'),
             'timeout' => null,
             'auth_mode' => null,
         ],
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('XMAIL_FROM_ADDRESS', 'dswd.focrg@dswd.gov.ph'),
+        'name' => env('XMAIL_FROM_NAME', 'DSWD Field Office Caraga'),
     ],
 
     /*
