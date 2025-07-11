f09304784c3991f42b5373ff9430a67ebf7c2fa0
Jowam <69232719+dioame@users.noreply.github.com>
2025-07-11 10:20:09 +0800

    updates

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('NEW_DB_HOST', '172.31.240.134'),
+            'host' => env('DB_HOST',env('NEW_DB_HOST', '172.31.240.134')),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
